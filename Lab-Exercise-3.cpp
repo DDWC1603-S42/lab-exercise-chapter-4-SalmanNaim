8ea563228054573de4a64a6cc7d2ba270d82d987
SalmanNaim <33915606+SalmanNaim@users.noreply.github.com>
2018-01-18 09:39:36 +0800

    dah siap program reverse number

--- Lab-Exercise-3.cpp
+++ Lab-Exercise-3.cpp
@@ -1,15 +1,22 @@
 //Display the number in reverse order
 //fix and complete the program below.
 
+#include<iostream>
+#include<conio.h>
+using namespace std;
 
-cout<<"user input a number: ";
-
-for(t=num;num!=0;num=num/10)
-{
-	r=num % 10;
-	sum=sum*10+r;
-
-}
-cout<<"The number in reverse order: "<<endl;
-
-}
+  int main()
+  {
+   int no,rev=0,r,a;
+   cout<<"Enter ascending number: ";
+   cin>>no;
+   a=no;
+   for(;no>0;)
+   {
+    r=no%10;
+    rev=rev*10+r;
+    no=no/10;
+   }
+  cout<<"\nReverse of "<<a<<" is: "<<rev;
+  getch();
+ }
