0d4445a06af851799a9cccd3b08bf784c89d1c2f
SalmanNaim <33915606+SalmanNaim@users.noreply.github.com>
2018-01-21 23:59:19 +0800

    exercise 4 updated

--- Lab-Exercise-4.cpp
+++ Lab-Exercise-4.cpp
@@ -1,24 +1,22 @@
-//Display the pattern using numebr repeating for a row
+//name: AHMAD SALMAN NAIM BIN MOHD ATAN   //matric no.: A17DW0712
+//Display the pattern using number repeating for a row
 //fix and complete the program below.
 
 
 #include <iostream>
-
+using namespace std;
 
 int main()
 {
 	cout<<"Display the pattern"<<endl;
 	cout<<"-------------------"<<endl;
 
-	i,j,rows;
-	cout<<"Input number of rows :"<<endl;
-
+	int i,j,rows;
+	cout<<"Input number of rows :";
+    cin>>rows;
 
 	for(i=1;i<=rows;i++)
-	{
-	for(j=1;j<=i;j++)
-		
-	cout<<endl;
+	{	
+	cout<<"new row "<<endl;
 	}
 }
-	
